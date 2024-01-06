c964282fd29bb94f0554e9bba9189f20b448c31b
みなぎし <114616680+minagishl@users.noreply.github.com>
2024-01-06 22:12:06 +0900

    ウェブUIをTailwindで仕上げました (#5)
    * css を Tailwind で書き直しました
    
    * 印刷プレビューなどでの表示が崩れる問題を修正
    
    * 開発用のボーダー消し忘れ
    
    * CSSが崩れる問題を修正
    
    * fix
    
    * コンフィグの設定ミス
    
    * 説明付き
    
    * 修正しました

--- src/utils/getOverpassResponse.ts
+++ src/utils/getOverpassResponse.ts
@@ -1,4 +1,4 @@
-import { Md5 } from "ts-md5";
+import { Md5 } from 'ts-md5';
 
 export const getOverpassResponse = async (overpassQuery: string) => {
   const overpassApiUrl = `https://z.overpass-api.de/api/interpreter?data=${encodeURIComponent(
@@ -35,7 +35,7 @@ export const getOverpassResponseJsonWithCache = async (
     try {
       window.localStorage.setItem(key, JSON.stringify(valueToStore));
     } catch (error) {
-      console.error("getOverpassResponseJsonWithCache error:", error);
+      console.error('getOverpassResponseJsonWithCache error:', error);
     }
     return json;
   };
@@ -43,10 +43,7 @@ export const getOverpassResponseJsonWithCache = async (
   const cache = window.localStorage.getItem(key);
   if (cache) {
     const valueFromStore = JSON.parse(cache);
-    if (
-      "resJson" in valueFromStore &&
-      unixtime - cacheSeconds < valueFromStore.unixtime
-    ) {
+    if ('resJson' in valueFromStore && unixtime - cacheSeconds < valueFromStore.unixtime) {
       return valueFromStore.resJson;
     } else {
       return await getAndCache();
